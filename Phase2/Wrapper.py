d3ed976f4cc3c1be0f606e1739569c721c5cdeba
fer <lfrecalde1@espe.edu.ec>
2025-03-15 23:42:43 -0400

    saving data properly

--- Phase2/Wrapper.py
+++ Phase2/Wrapper.py
@@ -64,13 +64,20 @@ def train(images, poses, camera_info, args):
     )
     # initialize a large loss value
     min_loss = float("inf")
+    print(images.shape)
+    print(poses.shape)
     # generate all rays
-    rays = generateBatch(images, poses, camera_info)
+    rays_total = generateBatch(images, poses, camera_info)
+    rays_warm = generateBatch(images[:, 100:300, 100:300, :], poses, camera_info)
     # calculate number of batches
 
-    num_batches = int(len(rays) / args.n_rays_batch)
-
     for epoch in tqdm(range(args.num_epochs)):
+        if epoch == 0:
+            rays = rays_warm
+        else:
+            rays = rays_total
+
+        num_batches = int(len(rays) / args.n_rays_batch)
         for i in tqdm(range(num_batches)):
             # model in training mode for gradients
             model.train()
@@ -200,15 +207,14 @@ def test(args, mode="test", epoch=0):
     imgs_path = os.path.join(args.logs_path, "Media", "Epoch" + str(epoch))
     if not os.path.exists(imgs_path):
         os.makedirs(imgs_path)
-
+    image_number = 5
     test_rays = generateBatch(
-        np.expand_dims(test_images[0], axis=0),
-        np.expand_dims(test_poses[0], axis=0),
+        np.expand_dims(test_images[image_number], axis=0),
+        np.expand_dims(test_poses[image_number], axis=0),
         camera_info,
     )
-    model = NeRFmodel(
-        embed_pos_L=10, embed_direction_L=4, flag_encoding=args.position_encoding
-    ).to(DEVICE)
+
+    model = NeRFmodel(embed_pos_L=10, embed_direction_L=4).to(DEVICE)
     model.load_state_dict(
         torch.load(os.path.join(args.checkpoint_path, "best_model.pt"))
     )
@@ -216,6 +222,12 @@ def test(args, mode="test", epoch=0):
     C_hat_list = []
     psnr_sum = 0.0
     test_start = time.time()
+    H, W, _ = test_images[0].shape
+
+    ## Original IMage
+    original_img = test_rays[:, 6:] * 255.0
+    original_img = original_img.reshape(H, W, 3)
+
     for i in range(0, len(test_rays), args.n_rays_batch):
         batch = test_rays[i : i + args.n_rays_batch]
         test_o = torch.tensor(batch[:, :3]).to(DEVICE)
@@ -229,31 +241,102 @@ def test(args, mode="test", epoch=0):
 
         psnr_sum += psnr.item()
 
-    H, W, _ = test_images[0].shape
     img = torch.cat(C_hat_list).numpy().reshape(H, W, 3) * 255.0
-
-    ssim = (
-        structural_similarity(
-            img[:, :, 0],
-            test_images[0][:, :, 0] * 255.0,
-            data_range=img[:, :, 0].max() - img[:, :, 0].min(),
-        )
-        + structural_similarity(
-            img[:, :, 1],
-            test_images[0][:, :, 1] * 255.0,
-            data_range=img[:, :, 1].max() - img[:, :, 1].min(),
-        )
-        + structural_similarity(
-            img[:, :, 2],
-            test_images[0][:, :, 2] * 255.0,
-            data_range=img[:, :, 2].max() - img[:, :, 2].min(),
-        )
-    ) / 3
-
+    image_bgr = img[..., ::-1]
+    image_bgr_original = original_img[..., ::-1]
     psnr = psnr_sum * args.n_rays_batch / len(test_rays)
     print(f"Iteration: {i}, PSNR: {psnr}, Avg time: {time.time()- test_start}")
 
-    cv2.imwrite(os.path.join(args.logs_path, "Media", "test_.png"), img)
+    cv2.imwrite(
+        os.path.join(
+            args.logs_path,
+            "Media",
+            "Nerf_test_" + str(image_number) + "_" + str(epoch) + ".png",
+        ),
+        image_bgr,
+    )
+    cv2.imwrite(
+        os.path.join(
+            args.logs_path,
+            "Media",
+            "Original_test_" + str(image_number) + "_" + str(epoch) + ".png",
+        ),
+        image_bgr_original,
+    )
+
+
+def compute_image(image_number, mode, model):
+
+    test_images, test_poses, camera_info = loadDataset(args.data_path, mode)
+    test_rays = generateBatch(
+        np.expand_dims(test_images[image_number], axis=0),
+        np.expand_dims(test_poses[image_number], axis=0),
+        camera_info,
+    )
+
+    C_hat_list = []
+    psnr_sum = 0.0
+    test_start = time.time()
+    H, W, _ = test_images[0].shape
+
+    ## Original IMage
+    original_img = test_rays[:, 6:] * 255.0
+    original_img = original_img.reshape(H, W, 3)
+
+    for i in range(0, len(test_rays), args.n_rays_batch):
+        batch = test_rays[i : i + args.n_rays_batch]
+        test_o = torch.tensor(batch[:, :3]).to(DEVICE)
+        test_d = torch.tensor(batch[:, 3:6]).to(DEVICE)
+        test_C_r = torch.tensor(batch[:, 6:]).to(DEVICE)
+        test_C_hat = render(model, test_o, test_d, args.tn, args.tf, args.n_sample)
+        loss, psnr = Loss(test_C_r, test_C_hat)
+        C_hat_list.append(test_C_hat.detach().cpu())
+        del test_o, test_d, test_C_r, test_C_hat
+        if psnr > 1e10:
+            continue
+
+        psnr_sum += psnr.item()
+
+    img = torch.cat(C_hat_list).numpy().reshape(H, W, 3) * 255.0
+    image_bgr = img[..., ::-1]
+    image_bgr_original = original_img[..., ::-1]
+
+    cv2.imwrite(
+        os.path.join(
+            args.logs_path,
+            "Media",
+            "Nerf_gif_" + str(image_number) + ".png",
+        ),
+        image_bgr,
+    )
+    cv2.imwrite(
+        os.path.join(
+            args.logs_path,
+            "Media",
+            "Original_gif_" + str(image_number) + ".png",
+        ),
+        image_bgr_original,
+    )
+    return img
+
+
+def gif_test(args, mode="test"):
+
+    model = NeRFmodel(embed_pos_L=10, embed_direction_L=4).to(DEVICE)
+    model.load_state_dict(
+        torch.load(os.path.join(args.checkpoint_path, "best_model.pt"))
+    )
+    model.eval()
+    images = []
+    for k in range(0, 100):
+        print("Computing Image", k)
+        images.append(compute_image(k, mode, model))
+    images = np.array(images)
+    print(images.shape)
+    images = images.astype(np.uint8)
+    path = os.path.join(args.logs_path, "Media", "Nerf_test" + ".gif")
+    frame_duration = 0.1  # duration in seconds for each frame
+    imageio.imwrite(path, images, duration=frame_duration)
 
 
 def main(args):
@@ -282,6 +365,7 @@ def main(args):
     # Path with object name
     path = os.path.join(args.data_path, args.object + "/")
     images, poses, camera_info = loadDataset(path, args.mode)
+    print(poses[0, :, :])
 
     #    # initialize logger
 
@@ -315,13 +399,16 @@ def main(args):
     elif args.mode == "test":
         print("Start testing")
         test(args)
+    elif args.mode == "gif":
+        print("Start gif")
+        gif_test(args)
 
 
 def configParser():
     parser = argparse.ArgumentParser()
     parser.add_argument("--data_path", default="./Data", help="dataset path")
     parser.add_argument("--object", default="ship", help="dataset path")
-    parser.add_argument("--mode", default="train", help="train | test | val")
+    parser.add_argument("--mode", default="train", help="train | test | val | gif")
     parser.add_argument(
         "--lrate", type=float, default=5e-4, help="training learning rate"
     )
--- Phase2/main_job_2.sh
+++ Phase2/main_job_2.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #SBATCH -A gli7
-#SBATCH -p short       # partition name
+#SBATCH -p long       # partition name
 #SBATCH -N 1             # number of nodes
 #SBATCH -c 32            # number of CPU cores
 #SBATCH --gres=gpu:2     # number of GPUs
